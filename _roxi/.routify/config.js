286bd8812cad9c2f88b322cba1b6e12b6b45fc46
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-08 23:09:52 +0200

    first commit

---
+++ _roxi/.routify/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": "_roxi/.routify",
+  "ignore": "",
+  "dynamicImports": false,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "extensions": [
+    "html",
+    "svelte",
+    "md"
+  ],
+  "distDir": "dist",
+  "plugins": {
+    "roxi/lib/routify-plugins/importPathsToMounts.js": {}
+  },
+  "started": "2020-06-08T21:07:24.774Z"
+}
