aaaaaf0d0cbabee68302aabadb66f9bfc9635dc7
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-26 21:24:18 +0200

    copy roxi example to template

---
+++ src/pages/blog/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page, layout } from "roxi/routify";
+  console.log($layout);
+</script>
+
+<h3>Posts</h3>
+{#each $layout.parent.children.find((node) => node.title === 'posts').children as child}
+  <a href={child.path}>{child.title}</a>
+{/each}
+<slot />
