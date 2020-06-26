b109549749f8344d3cb01d1ced0296ab2d658288
Wolfr <johan.ronsse@gmail.com>
2020-06-26 22:55:43 +0200

    A list of blog posts should semantically be a list.
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/pages/blog/index.svelte
+++ src/pages/blog/index.svelte
@@ -4,7 +4,11 @@
 </script>
 
 <h3>Posts</h3>
-{#each $layout.parent.children.find((node) => node.title === 'posts').children as child}
-  <a href={child.path}>{child.title}</a>
-{/each}
+
+<ul>
+    {#each $layout.parent.children.find((node) => node.title === 'posts').children as child}
+        <li><a href={child.path}>{child.title}</a></li>
+    {/each}
+</ul>
+
 <slot />
